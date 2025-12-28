8cb43073a9c410f05e4133d2c71b86d0d893f1c0
charan <charanramagiri2004@gmail.com>
2025-12-29 00:53:17 +0530

    uploaded resume and updated hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,15 +8,16 @@ function Hero() {
           Hi, I'm <span>Charan</span>
         </h1>
 
-        <h2>B.Tech Final Year Student | React Developer</h2>
+        <h2>B.Tech Final Year Student | Full Stack Developer</h2>
 
         <p>
-          I am a passionate frontend developer skilled in React, JavaScript,
-          HTML, and CSS. I love building clean and responsive web applications.
+            Aspiring Full Stack Developer with hands-on experience in React, JavaScript, Python, and Flask. Passionate about building clean, scalable, and user-focused web applications.
         </p>
 
+
         <div className="hero-buttons">
-          <button className="btn primary">View Resume</button>
+          <a href="/Charan_Ramagiri" target="_blank" rel="noopener noreferrer" className="btn primary"> View Resume </a>
+
           <button className="btn secondary">Contact Me</button>
         </div>
       </div>
